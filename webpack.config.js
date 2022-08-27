7fbdf3cdfd07bb47695927580adece08bf000305
Mikhail <gm3236@gmail.com>
2022-08-28 00:20:38 +0300

    1.3

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { Chunk } = require('webpack');
 
 const devServer = (isDev) => !isDev ? {} : {
     devServer: {
-      watchFiles: ["./src/*"],
+      watchFiles: ["./src/pages/blog.html"],
         open: true,
         hot: true,
         port: 8080,
@@ -13,17 +14,56 @@ const devServer = (isDev) => !isDev ? {} : {
 
 module.exports = ({develop}) => ({
   mode: develop ? 'development' : 'production',
-  entry: './src/index.js',
+  entry:{
+    index: './src/index.js',
+    blog: './src/blog.js',
+    post: './src/js/post.js',
+    about: './src/js/about.js',
+    contact: './src/js/contact.js',
+    prices: './src/js/contact.js'
+  }, 
+
+
+  
+  
 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'js/[name].js',
     assetModuleFilename: 'files/[hash][ext][query]'
   },
   
   plugins: [
       new HtmlWebpackPlugin({
-          template: './src/index.html'
+          filename: 'index.html',
+          template: './src/index.html',
+          chunks: ['index']
+      }),
+          
+      new HtmlWebpackPlugin({
+        filename: './pages/blog.html',
+        template: './src/pages/blog.html',
+        chunks: ['blog']
+      }),
+      new HtmlWebpackPlugin({
+        filename: './pages/post.html',
+        template: './src/pages/post.html',
+        chunks: ['post']
+      }),
+      new HtmlWebpackPlugin({
+        filename: './pages/about.html',
+        template: './src/pages/about.html',
+        chunks: ['about']
+      }),
+      new HtmlWebpackPlugin({
+        filename: './pages/contact.html',
+        template: './src/pages/contact.html',
+        chunks: ['contact']
+      }), 
+      new HtmlWebpackPlugin({
+        filename: './pages/prices.html',
+        template: './src/pages/prices.html',
+        chunks: ['prices']
       }),
       new MiniCssExtractPlugin({
           filename: './styles/main.css'
